a46eda0017ae920d61d704f5d3626c22c2c93521
gaur8126 <gaurlokesh1211@gmail.com>
2025-04-14 14:40:07 +0530

    common functionality added

---
+++ src/textSummarizer/utils/commomn.py
@@ -0,0 +1,31 @@
+import os 
+from box.exceptions import BoxValueError
+import yaml
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+from src.textSummarizer.logging import logger
+
+@ensure_annotations
+def read_yaml(path_to_yaml:Path)-> ConfigBox:
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+        
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+@ensure_annotations 
+def create_directories(path_to_directories:list,verbose=True):
+
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"created directories at: {path}")
